74a9a4d8b65cc7b289e0648c9c0624a5db5def17
rumiaqua <rumiaqua@hotmail.co.jp>
2015-12-07 15:40:02 +0900

    initial commit

---
+++ Framework/Siv3D/Main.cpp
@@ -0,0 +1,14 @@
+﻿
+# include <Siv3D.hpp>
+
+void Main()
+{
+	const s3d::Font font(30);
+
+	while (s3d::System::Update())
+	{
+		font(L"ようこそ、Siv3D の世界へ！").draw();
+
+		s3d::Circle(s3d::Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+	}
+}
